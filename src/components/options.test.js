014e580f827f896eb1dc20ce3e1c1ba352a9e09c
Justin Fry <mrjustinfry@gmail.com>
2018-12-05 13:05:10 -0500

    fixed options test

--- src/components/options.test.js
+++ src/components/options.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {shallow} from 'enzyme'; //don't forget to add "mount" for fullDOM
 
+import store from '../store';
 import Options from './options';
 
 describe('<LoginForm />', () => {
     it('Renders without crashing', () => {
-        shallow(<Options />);
+        shallow(<Options store={store} />);
     });
   });
